a393078d53fdb712f740109b4be084ad039e5e2e
Komodeo <onlythebest@gmail.com>
2022-03-27 20:40:14 -0700

    complete report

--- js/script.js
+++ js/script.js
@@ -15,31 +15,37 @@ images[1] = 'img/article/seminude-hikers.jpg';
 images[2] = 'img/article/nude-hot-springs-1.jpg';
 images[3] = 'img/article/man-sleeping-naked.jpg';
 
+//alt text array list
 alt[0] = "Great British Skinny Dip, Studland Beach";
 alt[1] = "Photograph of seminude hikers";
 alt[2] = "Illustration of woman at Nude Hot Springs";
 alt[3] = "Man sleeping naked";
 
+//title array list
 title[0] = "What nudists say about Wales's best nude beaches";
 title[1] = "Everybody Naked: Can Naturism become a Trend?";
 title[2] = "How a Visit to Nude Hot Springs Helped Me Confront My Fear of Aging";
 title[3] = "7 Reasons Why Sleeping Naked Means Sleeping Better";
 
+//author array list
 author[0] = "by Demi Roberts";
 author[1] = "by Nick & Lins";
 author[2] = "by Ashley Brooks";
 author[3] = "by Sara Novak";
 
+//teaser text array list
 teaser[0] = "It might still be winter, but cold water skinny dipping is very much on trend";
 teaser[1] = "What’s a trend? It’s basically something that suddenly becomes very popular.";
 teaser[2] = "A day spent bathing among naked strangers helped writer Ashley Brooks become more comfortable in her own skin.";
 teaser[3] = "And one reason why it’s actually gross.";
 
+//hyperlink array list
 link[0] = 'beaches.html';
 link[1] = 'trend.html';
 link[2] = 'fear.html';
 link[3] = 'sleep.html';
 
+//function to change the slider image
 function changeImg() {
     //add src to the slider in the index.html
     document.getElementById("slider").src = images[i];
@@ -52,12 +58,14 @@ function changeImg() {
     document.getElementById("dot-" + [i + 1]).style.backgroundColor = "#717171";
 }
 
+//function to change image when left button is pressed
 function leftArrow() {
     //Check if i is 0, then go to the last element of the array instead of go to -1 index
     (i == 0 ? i = images.length - 1 : i--);
     changeImg();
 }
 
+//function to change image when right button is pressed
 function rightArrow() {
     //Check if i is the last element of the array, then go to the first element of the array instead of exceeds array bound
     (i == images.length - 1 ? i = 0 : i++);
@@ -65,7 +73,7 @@ function rightArrow() {
 }
 
 //When the index page is loaded, run changeImg function to get the first image
-//Adapted from JF with help from https://stackoverflow.com/questions/9578348/best-way-to-execute-js-only-on-specific-page and https://stackoverflow.com/questions/17970734/how-to-call-window-load-event-on-specific-page
+//Adapted from JF with help from https://stackoverflow.com/questions/17970734/how-to-call-window-load-event-on-specific-page
 function loadIndex() {
     if (window.location.href.match('index.html')) {
         return changeImg();
